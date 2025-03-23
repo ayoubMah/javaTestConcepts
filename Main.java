a6a8ac77e2b426cad39c9b84a689ba68f6d8321a
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-03-23 01:50:15 +0100

    Read File with javaIO --> FileInputStream

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {3,7,1,9,5};
+        Arrays.sort(nums1);
+        for(int i = 0 ; i < nums1.length; i++ ){
+            System.out.println(nums1[i]);
+        }
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+
+        try{
+          fis = new FileInputStream("text.txt");
+          int byteData ;
+
+          while((byteData = fis.read()) != -1){
+            System.out.print((char)byteData);
+          }
+          System.out.println("\nFinish reading file");
+        }catch (IOException e){
+          System.err.println("Error reading file" + e.getMessage());
+          e.printStackTrace();
+
+        }finally{
+          try{
+            if(fis != null){
+              fis.close();
+            }
+          }catch(IOException closeException){
+            System.err.println("Error closing file" + closeException.getMessage());
+          }
+        }
+
+    }
+}
+
