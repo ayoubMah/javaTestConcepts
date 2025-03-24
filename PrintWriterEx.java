2b231fc93333c451dbefb7c521f1b42dba79a387
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-03-24 02:17:34 +0100

    PlayWith PrintWriter in progress

---
+++ PrintWriterEx.java
@@ -0,0 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrintWriterEx {
+  public static void main(String [] args){
+    PrintWriter pw = null ;
+
+    try{
+      
+    }
+
+  }
+}
