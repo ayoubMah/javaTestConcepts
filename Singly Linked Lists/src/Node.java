ec3fd10ea4c6fea69bc8df134df9e3c3a574936b
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-08-11 15:44:35 +0200

    play with singly node

---
+++ Singly Linked Lists/src/Node.java
@@ -0,0 +1,30 @@
+public class Node<E> {
+
+    private E data;
+    private Node<E> next;
+
+    public Node(E data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public String toString(){
+        return "("+data+"|"+ (next != null ?  next.data: "null");
+    }
+}
---
+++ Singly Linked Lists/src/SinglyLikedList.java
@@ -0,0 +1,2 @@
+public class SinglyLikedList<E>{
+}
