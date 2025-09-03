36d8675745117b80aa2e2973262fbcbbda0aec16
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-09-04 00:27:39 +0200

    delete head from cll

--- Singly Linked Lists/src/SinglyLinkedListPro.java
+++ Singly Linked Lists/src/SinglyLinkedListPro.java
@@ -1,5 +1,4 @@
 public class SinglyLinkedListPro<E> {
-    private NodeProMax<E> head = null;
     private NodeProMax<E> tail = null;
     private int size = 0;
 
@@ -15,7 +14,7 @@ public class SinglyLinkedListPro<E> {
 
     public E getFirst(){
         if (this.isEmpty()) return null;
-        return head.getElm();
+        return null;
     }
 
     public E getLast(){
@@ -24,23 +23,17 @@ public class SinglyLinkedListPro<E> {
     }
 
     public void addFirst(E elm){
-        head = new NodeProMax<>(elm, head);
-        if (size == 0) tail = head;
         size ++;
     }
 
     public void addLast(E elm){
         NodeProMax<E> newOne = new NodeProMax<>(elm, null);
-        if (size == 0) head = newOne;
-        else tail.setNext(newOne);
         tail = newOne;
         size++;
     }
 
     public E removeFirst(){
         if (isEmpty()) return null;
-        E answe = head.getElm();
-        head = head.getNext();
-        return answe;
+        return null;
     }
 }
